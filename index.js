afca3b6b280147f46ef7f94db6bdccd34dc20cf4
Your Name <you@example.com>
2022-05-28 12:04:15 +0600

    token added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { ObjectID } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,13 +11,113 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.megck.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.megck.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+async function run() {
+  try {
+    await client.connect();
+    const partCollection = client
+      .db("refrigerator_parts")
+      .collection("products");
+    const orderCollection = client
+      .db("refrigerator_parts")
+      .collection("orders");
+    const userCollection = client.db("refrigerator_parts").collection("users");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = partCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectID(id) };
+      const singleProduct = await partCollection.findOne(query);
+      res.send(singleProduct);
+    });
+
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.put("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "7d" }
+      );
+
+      res.send(result);
+    });
+
+    app.post("/order", verifyJWT, async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.get("/order", verifyJWT, async (req, res) => {
+      const query = {};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+
+      res.send(orders);
+    });
+
+    app.delete("/order/:email", async (req, res) => {
+      const id = req.params.email;
+      const filter = { email: email };
+      const result = await orderCollection.deleteOne(filter);
+
+      res.send(result);
+    });
+
+    console.log("Server is running");
+  } finally {
+  }
+}
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
